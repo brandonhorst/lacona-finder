5aee8881229c9fe9bd3ef948c26f1bcf5d5462c8
Brandon Horst <brandonhorst@gmail.com>
2015-11-10 05:38:45 -0500

    Update for release

--- src/index.jsx
+++ src/index.jsx
@@ -1,94 +1,157 @@
 /** @jsx createElement */
 
-import {createElement, Phrase, Source} from 'lacona-phrase'
-import open from 'open'
-import path from 'path'
-import {exec} from 'child_process'
-import Spotlight from 'LaconaSource-Spotlight'
+import {createElement, Phrase} from 'lacona-phrase'
+// import open from 'open'
+import Application from 'lacona-phrase-application'
+import PreferencePane from 'lacona-phrase-preference-pane'
+import URL from 'lacona-phrase-url'
+import File from 'lacona-phrase-file'
+import {RunningApplication, BrowserTab, OpenWindow} from 'lacona-phrase-system-state'
 
-// class Spotlight extends Phrase {
-//
-//   static get initialState() {return {files: []}}
-//
-//   compute(input) {
-//     if (input !== '' && !this.state.files[input]) {
-//       const args = ['-onlyin', this.props.dir, this.props.query]
-//       const opts = {stdout: 'pipe'}
-//       const child = spawn('mdfind', args, opts)
-//       var rl = readline.createInterface({
-//         input: child.stdout,
-//       })
-//       rl.on('line', line => {
-//         const files = this.state.files[input] || []
-//         const file = path.basename(line, '.app')
-//         this.setState({files: {[input]: files.concat(file)}})
-//       })
-//     }
-//
-//     const files = this.state.files[input] || []
-//     return files.map(file => ({text: file, value: file}))
-//   }
-//
-//   describe() {
-//     return <value compute={this.compute.bind(this)} limit={10} />
-//   }
-// }
-//
-class AppSource extends Source {
-  create() {
-    this.replaceData([])
-    setInterval(this.updateCache.bind(this), 60 * 1000)
-    this.updateCache()
+
+function tabCloseApplescript (value) {
+  if (value.app === 'Google Chrome') {
+    return `
+      tell application "Google Chrome"
+      	repeat with wi from 1 to count windows
+      		repeat with ti from 1 to count (window wi's tabs)
+      			if id of window wi's tab ti is ${value.id} then
+              close window wi's tab ti
+              return
+      			end if
+      		end repeat
+      	end repeat
+      end tell
+    `
+  } else if (value.app === 'Safari') {
+    const [winId, tabId] = value.id.split('-')
+    //TODO THIS DOES NOT WORK
+    return `
+      tell application "Safari"
+      	close window ${winId}'s tab ${tabId}
+      end tell
+    `
   }
 
-  updateCache() {
-    const child = exec(`mdfind -onlyin /Applications kind:application`, (err, stdout) => {
-      const resultString = stdout.toString('utf8')
-      const appPaths = resultString.split('\n').filter(str => str !== '')
-      const apps = appPaths.map(appPath => ({path: appPath, name: path.basename(appPath, '.app')}))
-      this.replaceData(apps)
+}
+
+export function execute (result) {
+  if (result.verb === 'open') {
+    result.items.forEach(item => {
+      if (item.url) {
+        global.openURL(item.url)
+      } else if (item.bundleId) {
+        global.launchApp(item.bundleId)
+      } else if (item.path) {
+        global.openFile(item.path)
+      }
+    })
+  } else if (result.verb === 'openin') {
+    result.items.forEach(item => {
+      result.apps.forEach(app => {
+        if (item.url) {
+          global.openURLInApp(item.url, app)
+        } else if (item.path) {
+          global.openFileInApp(item.path, app)
+        }
+      })
+    })
+  } else if (result.verb === 'switch') {
+      if (result.item.bundleId) {
+        global.launchApp(result.item.bundleId)
+      } else if (result.item.tabId) {
+        const script = tabSwitchApplescript(item.tabId)
+        global.applescript(script)
+      } else if (result.item.windowId) {
+
+      }
+
+  } else if (result.verb === 'quit') {
+    result.bundleIds.forEach(bundleId => {
+      global.quitApplication(bundleId, err => {
+        if (err) {
+          console.error(err)
+        }
+      })
+    })
+  } else if (result.verb === 'close') {
+    result.items.forEach(item => {
+      if (item.bundleId) {
+        global.launchApp(result.item.bundleId)
+      } else if (item.tabId) {
+
+        const script = tabCloseApplescript(result.item.tabId)
+        global.applescript(script)
+      }
     })
   }
 }
 
-class Application extends Phrase {
-  source() {
-    return {apps: <Spotlight directories={['/Applications']} query='kind:application' attributes={['kMDItemDisplayName']}/>}
-  }
-
-  describe() {
-    const apps = this.apps.map(app => ({text: app.kMDItemDisplayName, value: app.path}))
-
-    return <list fuzzy='true' items={apps} />
-  }
-}
-
-class OpenApp extends Phrase {
-  execute () {
-    open(result.path)
-  }
-
-  describe() {
+export class Sentence extends Phrase {
+  describe () {
     return (
-      <sequence>
-        <literal text='open ' category='action' />
-        <placeholder text='app' id='path' category='actor'>
-          <Application />
-        </placeholder>
-      </sequence>
+      <choice>
+        <sequence>
+          <literal text='open ' category='action' id='verb' value='open' />
+          <repeat unique={true} id='items' separator={<list items={[' and ', ', and ', ', ']} limit={1} category='conjunction' />}>
+            <choice>
+              <Application id='bundleId' />
+              <PreferencePane id='path' />
+              <URL splitOn={/\s|,/} id='url' />
+              <File id='path' />
+            </choice>
+          </repeat>
+        </sequence>
+        <sequence>
+          <literal text='open ' category='action' id='verb' value='openin' />
+          <repeat unique={true} id='items' separator={<list items={[' and ', ', and ', ', ']} limit={1} category='conjunction' />}>
+            <choice>
+              <URL splitOn={/\s|,/} id='url' />
+              <File id='path' />
+            </choice>
+          </repeat>
+          <list items={[' in ', ' using ', ' with ']} limit={1} category='conjunction' />
+          <repeat unique={true} id='apps' separator={<list items={[' and ', ', and ', ', ']} limit={1} category='conjunction' />}>
+            <Application />
+          </repeat>
+        </sequence>
+        <sequence>
+          <literal text='switch to ' category='action' id='verb' value='switch' />
+          <choice id='item'>
+            <RunningApplication id='bundleId' />
+            <OpenWindow id='windowId' />
+            <BrowserTab id='tabId' />
+          </choice>
+        </sequence>
+        <sequence>
+          <list items={['quit ', 'kill ']} category='action' id='verb' value='quit' />
+          <repeat unique={true} id='bundleIds' separator={<list items={[' and ', ', and ', ', ']} limit={1} category='conjunction' />}>
+            <RunningApplication />
+          </repeat>
+        </sequence>
+        <sequence>
+          <literal text='close ' category='action' id='verb' value='close' />
+          <repeat unique={true} id='items' separator={<list items={[' and ', ', and ', ', ']} limit={1} category='conjunction' />}>
+            <choice>
+              <RunningApplication id='bundleId' />
+              <OpenWindow id='windowId' />
+              <BrowserTab id='tabId' />
+            </choice>
+          </repeat>
+        </sequence>
+      </choice>
     )
   }
 }
 
-
-export default {
-  sentences: [OpenApp],
-  translations: [{
-    langs: ['en', 'default'],
-    information: {
-      title: 'Open Application',
-      description: 'Quickly launch applications on your computer',
-      examples: ['open Safari', 'open Contacts']
-    }
-  }]
-}
+// export default {
+//   sentences: [OpenApp],
+//   translations: [{
+//     langs: ['en', 'default'],
+//     information: {
+//       title: 'Open Application',
+//       description: 'Quickly launch applications on your computer',
+//       examples: ['open Safari', 'open Contacts']
+//     }
+//   }]
+// }
